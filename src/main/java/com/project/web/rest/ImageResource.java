b1624835f85ea9f053d7efbcd3bd77d5768f1a5a
Nil Panés <nil.panes@gmail.com>
2016-04-28 21:31:30 +0200

    Upload images

--- src/main/java/com/project/web/rest/ImageResource.java
+++ src/main/java/com/project/web/rest/ImageResource.java
@@ -15,8 +15,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.inject.Inject;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
@@ -34,13 +39,13 @@ import static org.elasticsearch.index.query.QueryBuilders.*;
 public class ImageResource {
 
     private final Logger log = LoggerFactory.getLogger(ImageResource.class);
-        
+
     @Inject
     private ImageRepository imageRepository;
-    
+
     @Inject
     private ImageSearchRepository imageSearchRepository;
-    
+
     /**
      * POST  /images -> Create a new image.
      */
@@ -89,7 +94,7 @@ public class ImageResource {
     public ResponseEntity<List<Image>> getAllImages(Pageable pageable)
         throws URISyntaxException {
         log.debug("REST request to get a page of Images");
-        Page<Image> page = imageRepository.findAll(pageable); 
+        Page<Image> page = imageRepository.findAll(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/images");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     }
@@ -139,4 +144,54 @@ public class ImageResource {
             .stream(imageSearchRepository.search(queryStringQuery(query)).spliterator(), false)
             .collect(Collectors.toList());
     }
-}
+
+    @RequestMapping(value = "/upload",
+        method = RequestMethod.POST,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    public void handleFileUpload(@RequestParam("file") MultipartFile file, @RequestParam("name") String name) {
+        log.debug("REST request to handleFileUpload");
+
+        File theDir = new File("./src/main/webapp/uploads");
+
+        byte[] bytes;
+
+        String nameSpace = "";
+
+        try {
+
+            if (!theDir.exists()) {
+                System.out.println("creating directory: /uploads");
+                boolean result = false;
+
+                try{
+                    theDir.mkdir();
+                    result = true;
+                }
+                catch(SecurityException se){
+                    //handle it
+                }
+                if(result) {
+                    System.out.println("DIR created");
+                }
+            }
+
+            file.getContentType();
+
+            //Get name of file
+            nameSpace = name;
+
+            //Create new file in path
+            BufferedOutputStream stream =
+                new BufferedOutputStream(new FileOutputStream(new File("./src/main/webapp/uploads/"+nameSpace)));
+
+            stream.write(file.getBytes());
+            stream.close();
+            log.debug("You successfully uploaded " + file.getName() + "!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    }
--- src/main/webapp/scripts/app/entities/space/space-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/space/space-dialog.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('project1App').controller('SpaceDialogController',
-    ['$scope', '$stateParams', 'entity', 'Space', 'Service', 'Image', 'Favorite', 'Review', 'Conversation', 'User', 'NgMap', '$state',
-        function($scope, $stateParams, entity, Space, Service, Image, Favorite, Review, Conversation, User, NgMap, $state) {
+    ['$scope', '$stateParams', 'entity', 'Space', 'Service', 'Image', 'Favorite', 'Review', 'Conversation', 'User', 'NgMap', '$state', 'Upload',
+        function($scope, $stateParams, entity, Space, Service, Image, Favorite, Review, Conversation, User, NgMap, $state, Upload) {
 
         $scope.space = entity;
         $scope.services = Service.query();
@@ -58,4 +58,86 @@ angular.module('project1App').controller('SpaceDialogController',
         }
 
 
+            $scope.uploadOK = false;
+            $scope.uploadStart = false;
+
+            $scope.uploadSong = function (files) {
+                $scope.filesUpload = files;
+                if (files != null) {
+                    for (var k = 0; k < files.length; k++) {
+                        uploadUsingUpload(files[k]);
+                    }
+                }
+            };
+
+            $scope.save = function () {
+                $scope.isSaving = true;
+                $scope.song.date_posted = new Date();
+                if ($scope.song.id != null) {
+                    Song.update($scope.song, onSaveSuccess, onSaveError);
+                } else {
+                    console.log($scope.picFile);
+                    if($scope.picFile == undefined){
+                        //$scope.uploadArt($scope.artworkFile);
+                        $scope.song.artwork = $rootScope.account.user_image;
+                    }else{
+                        var imageBase64 = $scope.croppedArtwork;
+                        var blob = dataURItoBlob(imageBase64);
+                        var file = new File([blob],"ds.jpg");
+                        //$scope.uploadArt($scope.artworkFile);
+
+                        $scope.uploadArt(file);
+                    }
+
+
+                    $scope.song.date_posted = new Date();
+                    Song.save($scope.song, onSaveSuccess, onSaveError);
+                }
+            };
+
+            function dataURItoBlob(dataURI) {
+                var binary = atob(dataURI.split(',')[1]);
+                var array = [];
+                for(var i = 0; i < binary.length; i++) {
+                    array.push(binary.charCodeAt(i));
+                }
+                return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+            }
+
+            $scope.uploadArt = function (file) {
+                $scope.formUpload = true;
+                if (file != null) {
+                    uploadUsingUploadArtwork(file)
+                }
+            };
+
+            function uploadUsingUploadArtwork(file) {
+                var songArtworkName = file.name.toLowerCase();
+                var ext = file.name.split('.').pop();
+
+                function randomString(length, chars) {
+                    var result = '';
+                    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+                    return result;
+                }
+
+                var rString = "artwork-"+randomString(15, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') + "." + ext;
+                $scope.song.artwork = "uploads/" + rString;
+
+                Upload.upload({
+                    url: 'api/upload',
+                    data: {file: file, name: rString}
+                }).then(function (resp) {
+
+                    console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+                    $scope.song.artwork = "uploads/" + rString;
+                }, function (resp) {
+                    console.log('Error status: ' + resp.status);
+                }, function (evt) {
+                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                    console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+                });
+
+
+            };
         }]);
