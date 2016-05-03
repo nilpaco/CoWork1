4b15f51dd74bcb4ba710edc396cd74c806832e9f
Nil Panés <nil.panes@gmail.com>
2016-05-03 17:11:00 +0200

    Upload single image.

--- src/main/webapp/scripts/app/entities/space/space-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/space/space-dialog.controller.js
@@ -70,6 +70,25 @@ angular.module('project1App').controller('SpaceDialogController',
                 });
             };
 
+            $scope.submit = function() {
+                    $scope.upload($scope.file);
+            };
+
+            $scope.upload = function (file) {
+                Upload.upload({
+                    url: 'api/upload',
+                    data: {file: file, 'name': 'hola'}
+                }).then(function (resp) {
+                    console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+                }, function (resp) {
+                    console.log('Error status: ' + resp.status);
+                }, function (evt) {
+                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                    console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+                });
+            };
+
+
 
 
         }]);
